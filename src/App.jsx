8d3c2b12a161992e7fbcdf074d79c116db7465ea
AntRkv <rakovnyc@gmail.com>
2024-11-17 00:51:09 -0500

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import "./App.css";
 
-const App = () => <h1>Weather Dashboard</h1>;
+const App = () => (
+  <div className="App">
+    <h1>Weather Dashboard</h1>
+  </div>
+);
 
 export default App;
